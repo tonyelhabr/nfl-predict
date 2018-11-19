9b1ab2c1fa617a56dd225cfb1e7eba3709572769
tonyelhabr <anthonyelhabr@gmail.com>
2018-11-19 05:25:43 -0600

    auto

--- R/git-sandbox.R
+++ R/git-sandbox.R
@@ -1,5 +1,7 @@
 
-system("git status")
-system("git add .")
-system("git commit -m 'auto update'")
-system("git push -u origin master")
+# system('git config --global user.email "anthonyelhabr@gmail.com"')
+# system('git config --global user.name "tonyelhabr"')
+system('git status')
+system('git add .')
+system('git commit -m "auto"')
+system('git push -u origin master')
