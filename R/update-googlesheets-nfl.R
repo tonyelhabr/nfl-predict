98b388b87b26334ac297d1c3423ec14ab311a42d
Tony ElHabr <anthonyelhabr@gmail.com>
2019-01-14 05:16:02 -0600

    auto commit

--- R/update-googlesheets-nfl.R
+++ R/update-googlesheets-nfl.R
@@ -2,31 +2,33 @@
 
 odds_nfl_tr_exist <- import_odds_nfl_tr()
 
-.wk <- 17L
+.wk <- 19L
 .wk_lag1 <- .wk - 1L
 
-# undebug(do_get_scores_nfl_espn)
-scores_lag1 <- do_get_scores_nfl_espn(wk = .wk_lag1)
-scores_lag1_trim <-
-  scores_lag1 %>%
-  select(tm_home, tm_away, pts_home, pts_away)
-scores_lag1_trim
-teproj::export_path(
-  scores_lag1_trim,
-  config$path_scores_lag1_nfl_temp
-)
-if(interactive()) {
-  file.show(config$path_scores_lag1_nfl_temp)
-}
+# scores_lag1 <- do_get_scores_nfl_espn(wk = .wk_lag1)
+# scores_lag1_trim <-
+#   scores_lag1 %>%
+#   select(tm_home, tm_away, pts_home, pts_away)
+# scores_lag1_trim
+# teproj::export_path(
+#   scores_lag1_trim,
+#   config$path_scores_lag1_nfl_temp
+# )
+# if(interactive()) {
+#   file.show(config$path_scores_lag1_nfl_temp)
+# }
 
 odds_nfl_tr_lag1_close <-
   odds_nfl_tr_exist %>% 
-  filter(wk == .wk) %>% 
+  # filter(wk == .wk) %>% 
+  # filter(date == max(date) | date == (max(date) - lubridate::days(1))) %>% 
+  filter(date == (max(date) - lubridate::days(7)) | date == (max(date) - lubridate::days(8))) %>% 
   mutate(gm = sprintf("%sv%s", tm_home, tm_away)) %>% 
   group_by(gm) %>% 
   filter(row_number(desc(timestamp_scrape)) == 1L) %>% 
   ungroup() %>% 
-  .arrange_gm_nfl()
+  # .arrange_gm_nfl()
+  arrange(time)
 odds_nfl_tr_lag1_close
 odds_nfl_tr_lag1_close_trim <-
   odds_nfl_tr_lag1_close %>% 
@@ -41,13 +43,16 @@ if(interactive()) {
 
 odds_nfl_tr_open <-
   odds_nfl_tr_exist %>%
-  filter(wk == .wk) %>%
+  # filter(wk == .wk) %>%
+  # filter(date == max(date) | date == (max(date) - lubridate::days(1))) %>% 
+  filter(date == (max(date) - lubridate::days(7)) | date == (max(date) - lubridate::days(8))) %>% 
   mutate(gm = sprintf("%sv%s", tm_home, tm_away)) %>%
   filter(!is.na(spread_home) & !is.na(total)) %>%
   group_by(gm) %>%
   filter(row_number(timestamp_scrape) == 1L) %>%
   ungroup() %>%
-  .arrange_gm_nfl()
+  # .arrange_gm_nfl()
+  arrange(time)
 odds_nfl_tr_open
 
 odds_nfl_tr_open_trim <-
